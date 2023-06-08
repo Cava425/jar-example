2dbc9a270decb2a91e3169490dfaa88085a9df88
xuyongshi <heliocc@163.com>
2023-06-08 23:17:38 +0800

    init

---
+++ src/main/java/com/example/jar/JarExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarExampleApplication.class, args);
+    }
+
+}
